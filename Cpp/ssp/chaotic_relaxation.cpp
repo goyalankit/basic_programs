f1b0f0d02d0fad0da072e9ef60e8848c3b5844d0
Ankit Goyal <ankit3goyal@gmail.com>
2013-10-06 09:07:10 -0500

    adding seq chaotic relaxation

--- Cpp/ssp/chaotic_relaxation.cpp
+++ Cpp/ssp/chaotic_relaxation.cpp
@@ -5,6 +5,9 @@
 #include<cmath>
 #include<sys/time.h>
 #include <algorithm>
+#include<queue>
+#include<tr1/unordered_set>
+#include<tr1/unordered_map>
 
 using namespace std;
 
@@ -18,37 +21,20 @@ struct Edge
     int weight;
 };
 
-void print_maximal_matching(vector< vector<Edge> > working_set){
-  for(int i=0; i< working_set.size(); i++){
-    for(int j=0; j < working_set[i].size();j++){
-      cout << i << " " << working_set[i][j].source << " -> " << working_set[i][j].destination << endl;
+
+bool bucket_not_empty(vector < tr1::unordered_set <int> >  buckets){
+  for(int i=0; i < buckets.size(); i++){
+    if(buckets[i].size()!=0){
+      return true;
     }
   }
+  return false;
 }
 
 void print_shortest_path(int dist[]){
   for(int i=0; i< vCount ; i++){
     cout << i << " " << dist[i] << endl;
   }
-}
-
-bool isEmpty(vector<int> ith_bucket){
-  cout << "bucket size in empty " << ith_bucket.size() << endl;
-  for(int i=0; i< ith_bucket.size(); i++){
-    cout << "entered" << endl;
-    if(ith_bucket[i] != -1){
-      return false;
-    }
-  }
-  return true;
-}
-
-int relax_edge(int dist[], Edge edge){
-  if(dist[edge.source] + edge.weight < dist[edge.destination]){
-    dist[edge.destination] = dist[edge.source] + edge.weight;
-  }
-  cout << "edge relaxed"<<endl;
-  return edge.destination;
 }
 
 
@@ -58,6 +44,7 @@ int main(int argc, char **argv){
     exit(0);
   }
 
+  int delta = 10;
 
   int src, dst, wght;
   sourceNode = atoi(argv[2]);
@@ -68,63 +55,52 @@ int main(int argc, char **argv){
   ifstream read(filename);
   read >> vCount >> nEdges;
 
-  vector< vector<Edge> > graph(vCount);
-  vector< vector<int> > buckets(2);
+  //  vector< vector<int> > buckets(2);
+  vector < vector<int> > outVertices(vCount);
+  vector< tr1::unordered_map<int, int> > weight(vCount);
 
-  hash<<int>> weights;
+  int maxWeight = 0;
+
   while(read >> src >> dst >> wght){
-    Edge edge;
-    edge.destination = dst;
-    edge.weight = wght;
-    edge.source = src;
-    graph[src].push_back(edge);
-    
+    weight[src][dst] = wght;
+    outVertices[src].push_back(dst);
+
+    if(wght > maxWeight){
+      maxWeight = wght;
+    }
   }
 
-  cout << "checking graph" << graph[0][0].source << endl;
 
   int dist[vCount];
   for(int n=0;n<vCount;n++){
-    dist[n]=99;
+    dist[n]=maxWeight + 10;
   }
 
   dist[sourceNode] = 0;
+  cout << maxWeight;
+  tr1::unordered_set <int> bucket;
+  //  vector< queue<int> > buckets(vCount);
 
   int node, destination, new_bucket_number;
-  int delta = 3;
   bool relaxed = false;
   Edge edge;
-  cout << "bucket size " << buckets.size() << endl;
-  buckets[0].push_back(sourceNode);
+  cout << "bucket size " << bucket.size() << endl;
+  bucket.insert(sourceNode);
 
-  for(int i=0; i< buckets.size(); i++){
-    while(!isEmpty(buckets[i])){
-      cout << "is empty checked for bucket " << i << endl;
-      for(int j=0; j< buckets[i].size(); j++){
-        node = buckets[i][j];
-        buckets[i][j] = -1;
-        for(int k=0; k < graph[node].size(); k++){
-          relaxed = false;
-          edge = graph[node][k];
-          cout  << "running for source = " << graph[node][k].source << " and destination= " << edge.destination << endl;
-          if(dist[edge.source] + edge.weight < dist[edge.destination]){
-            dist[edge.destination] = dist[edge.source] + edge.weight;
-            relaxed = true;
-            destination = edge.destination;
-          }
+  vector<int> requests, deletedNodes;
+    while(!bucket.empty()){
+      node = (*bucket.begin());
+      bucket.erase(node);
+      cout << "running for node " << node;
 
-          //destination = relax_edge(dist, graph[node][k]);
-          cout << "new distance" << dist[destination] << endl;
-          if(relaxed){
-            new_bucket_number = dist[destination] % delta;
-            if(buckets.size() < new_bucket_number ){
-              buckets.resize(new_bucket_number + 1);
-            }
-            buckets[new_bucket_number].push_back(destination);
-          }
+      for(int i = 0; i < outVertices[node].size(); i++ ){
+         if(dist[node] + weight[node][outVertices[node][i]] < dist[outVertices[node][i]]){
+          dist[outVertices[node][i]] = dist[node] + weight[node][outVertices[node][i]];
+          int y = dist[outVertices[node][i]];
+          bucket.insert(outVertices[node][i]);
         }
       }
-    }
+
   }
   print_shortest_path(dist);
   return 0;
