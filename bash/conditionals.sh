811f6b01b38297bf29d3c99fba0e48d8f9a4efb2
Ankit Goyal <ankit3goyal@gmail.com>
2013-12-07 12:39:11 -0600

    conditionals in bash

---
+++ bash/conditionals.sh
@@ -0,0 +1,44 @@
+#/bin/bash
+#different syntax for conditional statements
+
+variable=2
+
+echo "----------------------------------------"
+echo "Type of conditionals supported by shell!"
+echo "----------------------------------------"
+
+if ( test  "$variable" == 2 ); then
+    echo "1:   (test \"\$variable\" == 2) condition passed"
+fi
+
+if (( "$variable" == 2 )); then
+    echo "2:   (( \"\$variable\" == 2)) condition passed"
+fi
+
+#if ("$variable" == 2); then
+    echo "3:   ( \"\$variable\" == 2 ) condition NOT passed"
+#fi
+
+if [ "$variable" == 2 ]; then
+    echo "4:   [ \"\$variable\" == 2 ] condition passed"
+fi
+
+if test  "$variable" == 2; then
+    echo "5:   test \"\$variable\" == 2 condition passed"
+fi
+
+#An awesome way! The one liner
+[ "$variable" == 2 ] &&  echo "6:   [ \$variable == 2 ] && echo condition passed"
+
+myString="/etc/rc2.d"
+
+#Regular Expressions.
+if [[ $myString =~ /etc/rc\d*..d ]]; then
+    echo "6:  [[ ]] brackets and =~ can be used to match regular expressions"
+fi
+
+echo "--------------------------"
+echo "other conditional options!"
+echo "--------------------------"
+
+if [ -z "$MY_ENV" ]; then echo "empty or unset"; fi
