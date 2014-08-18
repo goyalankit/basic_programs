7bf129bf38e32a3fd2ad89d28985a7d8c9e97dda
Ankit Goyal <ankit3goyal@gmail.com>
2014-08-18 16:18:06 -0500

    adding recursion problems

---
+++ Cpp/recursion/telephone_words.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+#define NUMBER_LENGTH 3
+
+// doesn't handle special case of 7 and 9. they don't
+// have successive characters on them
+char getCharKey(int telephoneKey, int place) {
+  int base_a = (int)'A';
+  int base_zero = '0';
+  if (telephoneKey == 1 || telephoneKey == 0){
+    return (char)(base_zero + telephoneKey);
+  }
+  return (char)(base_a + (telephoneKey - 2) * 3 + place-1);
+}
+
+void getTeleWord(int in[], string &out, int next){
+  if (next >= NUMBER_LENGTH) {
+    std::cout << out << std::endl;
+    return;
+  }
+
+  for (int i = 1; i < 4; ++i) {
+    string out2 = out;
+    out2 += getCharKey(in[next], i);
+    getTeleWord(in, out2, next+1);
+    if (in[next] == 1 || in[next] == 0){
+      return;
+    }
+  }
+}
+
+int main(void) {
+  int in[NUMBER_LENGTH] = {1,2,3};
+  string out = "";
+  getTeleWord(in, out, 0);
+  return EXIT_SUCCESS;
+}
